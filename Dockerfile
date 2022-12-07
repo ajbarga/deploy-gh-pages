a4deab4bf1ad1956c634e27a241942476829e9a7
Alex Barga <99690046+ajbarga@users.noreply.github.com>
2022-12-07 16:08:35 -0500

    Rebuild action on shell

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,7 @@
-FROM python:3.10
+FROM node:18
 
 COPY ./entrypoint.sh /entrypoint.sh
-COPY ./main.py /main.py
 
-RUN pip install --no-cache-dir pygithub==1.55 &&\
-    chmod +x /entrypoint.sh
+RUN chmod +x /entrypoint.sh
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,60 @@
 #!/bin/sh
 
-python /main.py "$TOKEN" "$REPO" "$PR_NUMBER" "$MERGE"
+SRC="SRC-BRANCH"
+DEST="DEPLOY-BRANCH"
+LOCAL="LOCAL"
+
+git config --global user.name "${ACTOR}"
+git config --global user.email "${ACTOR}@no-reply.github.com"
+
+git_base="https://${GITHUB_TOKEN}@github.com"
+
+# Clone src repo
+git clone ${git_base}/${REPO}.git ${SRC}
+
+# Checkout main branch
+cd ${SRC}
+git checkout -q -f ${MAIN}
+
+# Build deployment
+npm ci
+npm run build
+
+cd ..
+
+# Clone sync-ing repository
+git clone ${git_base}/${REPO}.git ${DEST}
+
+# Checkout branch
+cd ${DEST}
+git checkout -q -f ${DEPLOY}
+git checkout -q -f -b ${LOCAL}
+cd ..
+
+# Copy files from template repo
+cp -rf ${SRC}/${BUILD}/* ${DEST} || true
+
+# Navigate to sync-ing repo
+cd ${DEST}
+git add -f --all > /dev/null
+git commit -q -m "Deploy files from ${MAIN} branch to ${DEPLOY} branch"
+
+# Push changes to remote repository
+git push -f -q -u origin ${DEPLOY}
+
+# Create or edit AutoSync Pr
+gh pr create \
+    --repo ${REPO} \
+    --head ${LOCAL} \
+    --base ${DEPLOY}
+    --title "${PR_TITLE}" \
+    --body "Deployment PR created for @${ACTOR}." \
+    --label "deploy" > /dev/null || \
+gh pr edit ${LOCAL} \
+    --repo ${REPO} \
+    --title "${PR_TITLE}" > /dev/null
+
+# Delete Sync-ed Repo, Sleep to avoid API call overload
+cd ..
+rm -rf ${SRC}
+rm -rf ${DEST}
